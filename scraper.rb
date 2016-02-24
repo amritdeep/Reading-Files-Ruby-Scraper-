dea1c9c8c7df8de9bf1663a0d6eb96cf9f09dfb8
amritdeep <amritdeepdhungana@hotmail.com>
2016-02-24 10:47:20 -0600

    add zip file

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 require 'pry'
 require 'csv'
+require 'zipruby'
 
 zip = []
 
@@ -7,6 +8,12 @@ CSV.foreach("example2.csv") do |row|
 	zip << row[2]
 end
 
-
+Zip::Archive.open('Fund IIQ 4Q15 PCAP PDFs Q (2).zip') do |ar|
+	binding.pry
+   # ar.fopen('article.txt') do |f|
+   #    content = f.read
+   #    # do sth
+   end
+end
 
 
