0d4a669446912e1597a86a78f80e7c2572077271
amritdeep <amritdeepdhungana@hotmail.com>
2016-02-24 12:16:25 -0600

    handle nil value and comment unwanted code

--- scraper.rb
+++ scraper.rb
@@ -3,34 +3,37 @@ require 'pry'
 require 'csv'
 require 'zip'
 
-
-zip = []
+name = []
 file_name = []
 zipfile_name = "Fund IIQ 4Q15 PCAP PDFs Q.zip"
 
+## Move Name column data from sheet to name array
 CSV.foreach("example2.csv") do |row|
-	zip << row[2]
+	name << row[2]
 end
 
+## Move file name from zip file
 Zip::File.open('Fund IIQ 4Q15 PCAP PDFs Q.zip') do |zip_file|
   # Handle entries one by one
   zip_file.each do |entry|
-    if entry.directory?
-      puts "#{entry.name} is a folder!"
-    elsif entry.symlink?
-      puts "#{entry.name} is a symlink!"
-    elsif entry.file?
-      puts "#{entry.name} is a regular file!"
-    else
-      puts "#{entry.name} is something unknown, oops!"
-    end
+    # if entry.directory?
+    #   puts "#{entry.name} is a folder!"
+    # elsif entry.symlink?
+    #   puts "#{entry.name} is a symlink!"
+    # elsif entry.file?
+    #   puts "#{entry.name} is a regular file!"
+    # else
+    #   puts "#{entry.name} is something unknown, oops!"
+    # end
 
-    name = entry.name.gsub("KPEO II(Q), LP - 4Q15 Capital Statements - ", "")
-    pdf = name.gsub(".pdf", "")
+    entry_name = entry.name.gsub("KPEO II(Q), LP - 4Q15 Capital Statements - ", "")
+    entry_pdf = entry_name.gsub(".pdf", "")
 
-    file_name << pdf
+    file_name << entry_pdf
   end
 end
 
+name = name.compact
 
+puts name
 
